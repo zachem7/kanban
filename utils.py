5c4f4d2067a4034221feeb9bb1812c3c627ba28e
zachem7 <abjiba110@gmail.com>
2025-10-31 17:26:18 +0300

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import Session
-from .models import Task, TaskLog
-
-def create_task_log(db: Session, task: Task, message: str):
-    log = TaskLog(task_id=task.id, message=message)
-    db.add(log)
-    db.commit()
-    db.refresh(log)
-    return log
